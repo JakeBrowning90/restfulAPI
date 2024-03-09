fc80f2060894a78ce66aa9da2ed6c0b7d3202ea7
Jake Browning <jacobbrowning90@gmail.com>
2024-03-09 16:47:39 +0000

    End of tutorial

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express  from 'express';
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
 import models from './models;';
 import routes from './routes';
 
@@ -21,70 +21,6 @@ app.use((req, res, next) => {
 app.use('/session', routes.session);
 app.use('/users', routes.user);
 app.use('/messages', routes.message);
-
-app.get('/session', (req, res) => {
-    return res.send(req.context.models.users[req.context.me.id]);
-});
-
-// app.get('/users', (req, res) => {
-//     return res.send('GET HTTP method on user resource');
-// });
-
-app.get('/users', (req, res) => {
-    return res.send(Object.values(req.context.models.users));
-});
-
-app.get('/users/:userId', (req, res) => {
-    return res.send(req.context.models.users[req.params.userId]);
-});
-
-// app.post('/users', (req, res) => {
-//     return res.send('POST HTTP method on user resource');
-// });
-
-app.put('/users/:userId', (req, res) => {
-    return res.send(
-        `PUT HTTP method on user/${req.params.userId} resource`,
-    );
-});
-
-app.delete('/users/:userId', (req, res) => {
-    return res.send(
-        `DELETE HTTP method on user/${req.params.userId} resource`
-    );
-});
-
-app.get('/messages', (req, res) => {
-    return res.send(Object.values(req.context.models.messages));
-});
-
-app.get('/messages/:messageId', (req, res) => {
-    return res.send(req.context.models.messages[req.params.messageId]);
-});
-
-app.post('/messages', (req, res) => {
-    const id = uuidv4();
-    const message = {
-        id,
-        text: req.body.text,
-        userId : req.context.me.id,
-    };
-
-    req.context.models.messages[id] = message;
-
-    return res.send(message);
-});
-
-app.delete('/messages/:messageId', (req, res) => {
-    const {
-        [req.params.messageId]: message,
-        ...otherMessages
-    } = req.context.models.messages;
-
-    req.context.models.messages = otherMessages;
-
-    return res.send(message);
-});
 
 app.listen(process.env.PORT, () =>
     console.log(`Example app listening on port ${process.env.PORT}.`),
