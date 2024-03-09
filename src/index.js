13550a872bc4ecdeb33d27b3b98b0bc93defff5b
Jake Browning <jacobbrowning90@gmail.com>
2024-03-09 12:06:16 +0000

    Create user route file

--- src/index.js
+++ src/index.js
@@ -1,18 +1,46 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express  from 'express';
+import { v4 as uuidv4 } from 'uuid';
+import models from './models;';
+import routes from './routes';
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use((req, res, next) => {
+    req.context = {
+        models,
+        me: models.users[1],
+    };
+    next();
+});
+
+app.use('/session', routes.session);
+app.use('/users', routes.user);
+app.use('/messages', routes.message);
+
+app.get('/session', (req, res) => {
+    return res.send(req.context.models.users[req.context.me.id]);
+});
+
+// app.get('/users', (req, res) => {
+//     return res.send('GET HTTP method on user resource');
+// });
 
 app.get('/users', (req, res) => {
-    return res.send('GET HTTP method on user resource');
+    return res.send(Object.values(req.context.models.users));
 });
 
-app.post('/users', (req, res) => {
-    return res.send('POST HTTP method on user resource');
+app.get('/users/:userId', (req, res) => {
+    return res.send(req.context.models.users[req.params.userId]);
 });
+
+// app.post('/users', (req, res) => {
+//     return res.send('POST HTTP method on user resource');
+// });
 
 app.put('/users/:userId', (req, res) => {
     return res.send(
@@ -24,6 +52,38 @@ app.delete('/users/:userId', (req, res) => {
     return res.send(
         `DELETE HTTP method on user/${req.params.userId} resource`
     );
+});
+
+app.get('/messages', (req, res) => {
+    return res.send(Object.values(req.context.models.messages));
+});
+
+app.get('/messages/:messageId', (req, res) => {
+    return res.send(req.context.models.messages[req.params.messageId]);
+});
+
+app.post('/messages', (req, res) => {
+    const id = uuidv4();
+    const message = {
+        id,
+        text: req.body.text,
+        userId : req.context.me.id,
+    };
+
+    req.context.models.messages[id] = message;
+
+    return res.send(message);
+});
+
+app.delete('/messages/:messageId', (req, res) => {
+    const {
+        [req.params.messageId]: message,
+        ...otherMessages
+    } = req.context.models.messages;
+
+    req.context.models.messages = otherMessages;
+
+    return res.send(message);
 });
 
 app.listen(process.env.PORT, () =>
